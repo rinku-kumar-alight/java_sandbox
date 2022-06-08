0478e91e4085aeb18f077e8ba6208f39c190d598
Rinku Kumar <rinku.kumar.2@alight.com>
2022-06-08 17:59:31 +0530

    Adding maven file

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -7,6 +7,11 @@ public class CalculatorTest {
     @Test
     public void add() {
         var obj1 = new Calculator();
-        assertEquals(7, obj1.add(3, 4));
+        assertEquals(8, obj1.add(4, 4));
+    }
+    @Test
+    public void testSub(){
+        var obj2 = new Calculator();
+        assertEquals(6, obj2.sub(11, 5));
     }
 }
